66767fe3c980b2d01ca88cccb93a929a70483ee1
Shristi Maharjan <maharjanshristi60@yahoo.com>
2021-09-01 23:07:47 -0500

    PSQL Database added 3rd user

--- db/seed.js
+++ db/seed.js
@@ -41,9 +41,11 @@ async function createTables() {
       console.log("Starting to create users...");
   
       const albert = await createUser({ username: 'albert', password: 'bertie99' });
-      const sandra = await createUser({ username: 'sandra', password: 'glamgal' });
+     const sandra = await createUser({ username: 'sandra', password: '2sandy4me' });
+    await createUser({ username: 'glamgal', password: 'soglam' });
 
-      console.log(sandra);
+      console.log(albert);
+      console.log (sandra);
   
       console.log("Finished creating users!");
     } catch(error) {
